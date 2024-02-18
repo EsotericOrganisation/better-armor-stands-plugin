b30a21756663e8ef7957395ef216b7fa9bea4b2d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-02-18 19:51:05 +0000

    Cleanup

--- src/main/java/org/rolypolyvole/armourstandarms/listener/ArmourStandRightClickListener.java
+++ src/main/java/org/rolypolyvole/armourstandarms/listener/ArmourStandRightClickListener.java
@@ -1,6 +1,5 @@
 package org.rolypolyvole.armourstandarms.listener;
 
-import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.ArmorStand;
