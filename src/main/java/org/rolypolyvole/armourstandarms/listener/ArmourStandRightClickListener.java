0b2dd65bd2e603e88226e652ffb7b299a705a255
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-02-18 18:04:26 +0000

    Implement pose changing

--- src/main/java/org/rolypolyvole/armourstandarms/listener/ArmourStandRightClickListener.java
+++ src/main/java/org/rolypolyvole/armourstandarms/listener/ArmourStandRightClickListener.java
@@ -1,26 +1,49 @@
 package org.rolypolyvole.armourstandarms.listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.NotNull;
+import org.rolypolyvole.armourstandarms.BetterArmourStandsPlugin;
+import org.rolypolyvole.armourstandarms.manager.PoseManager;
 
 public class ArmourStandRightClickListener implements Listener {
+
+    private final BetterArmourStandsPlugin plugin;
+
+     public ArmourStandRightClickListener(BetterArmourStandsPlugin plugin) {
+         this.plugin = plugin;
+     }
+
     @EventHandler
-    public void onArmourStandClicked(@NotNull PlayerInteractAtEntityEvent event) {
+    public void onArmourStandClick(@NotNull PlayerInteractAtEntityEvent event) {
         Player player = event.getPlayer();
 
         if (!(event.getRightClicked() instanceof ArmorStand stand)) return;
         if (!player.isSneaking()) return;
 
-        if (player.getInventory().getItemInMainHand().getType().equals(Material.SMOOTH_STONE_SLAB)) {
-            stand.setBasePlate(true);
-            player.getInventory().setItemInMainHand(null);
+        PlayerInventory inventory = player.getInventory();
+        ItemStack mainHandItem = inventory.getItemInMainHand();
+        Material itemMaterial = mainHandItem.getType();
 
-            event.setCancelled(true);
+        if (itemMaterial.equals(Material.AIR)) {
+            PoseManager poseManager = plugin.getPoseManager();
+
+            int poseIndex = poseManager.getPoseIndex(stand) % poseManager.getPoses().size();
+
+            Bukkit.getLogger().info("poseIndex = " + poseIndex);
+
+            poseManager.setPose(stand, (poseIndex + 1) % poseManager.getPoses().size());
+        } else if (itemMaterial.equals(Material.SMOOTH_STONE_SLAB)) {
+            stand.setBasePlate(true);
+            inventory.setItemInMainHand(null);
+
             return;
         }
 
---
+++ src/main/java/org/rolypolyvole/armourstandarms/manager/PoseManager.java
@@ -0,0 +1,86 @@
+package org.rolypolyvole.armourstandarms.manager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.util.EulerAngle;
+import org.jetbrains.annotations.NotNull;
+import org.rolypolyvole.armourstandarms.BetterArmourStandsPlugin;
+
+import java.util.ArrayList;
+
+public class PoseManager {
+
+    private final ArrayList<ArrayList<ArrayList<Double>>> poses;
+
+    public ArrayList<ArrayList<ArrayList<Double>>> getPoses() {
+        return poses;
+    }
+
+    public PoseManager(@NotNull BetterArmourStandsPlugin plugin) {
+        poses = (ArrayList<ArrayList<ArrayList<Double>>>) plugin.getConfig().getList("poses");
+        assert poses != null;
+
+        for (ArrayList<ArrayList<Double>> poseRotations : poses) {
+            for (ArrayList<Double> componentRotations : poseRotations) {
+                componentRotations.replaceAll(Math::toRadians);
+            }
+        }
+    }
+
+    public int getPoseIndex(@NotNull ArmorStand armorStand) {
+        EulerAngle bodyPose = armorStand.getBodyPose();
+        EulerAngle headPose = armorStand.getHeadPose();
+        EulerAngle leftArmPose = armorStand.getLeftArmPose();
+        EulerAngle leftLegPose = armorStand.getLeftLegPose();
+        EulerAngle rightArmPose = armorStand.getRightArmPose();
+        EulerAngle rightLegPose = armorStand.getRightLegPose();
+
+        ArrayList<ArrayList<Double>> rotations = new ArrayList<>();
+
+        rotations.add(toRotationsArray(bodyPose));
+        rotations.add(toRotationsArray(headPose));
+        rotations.add(toRotationsArray(leftArmPose));
+        rotations.add(toRotationsArray(leftLegPose));
+        rotations.add(toRotationsArray(rightArmPose));
+        rotations.add(toRotationsArray(rightLegPose));
+
+        Bukkit.getLogger().info("rotations = " + rotations);
+
+        for (int i = 0; i < poses.size(); i++) {
+            ArrayList<ArrayList<Double>> poseRotations = poses.get(i);
+
+            Bukkit.getLogger().info("poseRotations = " + poseRotations);
+
+            if (poseRotations.equals(rotations)) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    public void setPose(@NotNull ArmorStand stand, int poseIndex) {
+        ArrayList<ArrayList<Double>> poseRotations = poses.get(poseIndex);
+
+        stand.setBodyPose(toEulerAngle(poseRotations.get(0)));
+        stand.setHeadPose(toEulerAngle(poseRotations.get(1)));
+        stand.setLeftArmPose(toEulerAngle(poseRotations.get(2)));
+        stand.setLeftLegPose(toEulerAngle(poseRotations.get(3)));
+        stand.setRightArmPose(toEulerAngle(poseRotations.get(4)));
+        stand.setRightLegPose(toEulerAngle(poseRotations.get(5)));
+    }
+
+    public ArrayList<Double> toRotationsArray(@NotNull EulerAngle eulerAngle) {
+        ArrayList<Double> list = new ArrayList<>();
+
+        list.add(eulerAngle.getX());
+        list.add(eulerAngle.getY());
+        list.add(eulerAngle.getZ());
+
+        return list;
+    }
+
+    public EulerAngle toEulerAngle(@NotNull ArrayList<Double> rotations) {
+        return new EulerAngle(rotations.get(0), rotations.get(1), rotations.get(2));
+    }
+}
