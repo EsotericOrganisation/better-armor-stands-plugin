079b39c09be129e23b4a206dab754eb21db1f589
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-02-18 18:46:13 +0000

    Change recipe

--- src/main/java/org/rolypolyvole/armourstandarms/BetterArmourStandsPlugin.java
+++ src/main/java/org/rolypolyvole/armourstandarms/BetterArmourStandsPlugin.java
@@ -4,19 +4,19 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.rolypolyvole.armourstandarms.listener.ArmourStandPlaceListener;
 import org.rolypolyvole.armourstandarms.listener.ArmourStandRightClickListener;
 import org.rolypolyvole.armourstandarms.manager.PoseManager;
-
-import java.util.Iterator;
+import org.rolypolyvole.armourstandarms.util.RecipeUtil;
 
 public final class BetterArmourStandsPlugin extends JavaPlugin {
 
     private PoseManager poseManager;
+
+    private final NamespacedKey baseplateLessArmourStandRecipeKey = new NamespacedKey(this, "baseplate_less_armour_stand");
 
     public PoseManager getPoseManager() {
         return poseManager;
@@ -37,21 +37,16 @@ public final class BetterArmourStandsPlugin extends JavaPlugin {
         PluginManager pluginManager = Bukkit.getPluginManager();
 
         pluginManager.registerEvents(new ArmourStandPlaceListener(), this);
-
         pluginManager.registerEvents(new ArmourStandRightClickListener(this), this);
 
+        RecipeUtil.removeRecipe(Material.ARMOR_STAND);
+
         ItemStack armourStand = new ItemStack(Material.ARMOR_STAND, 1);
-        NamespacedKey recipeKey = new NamespacedKey(this, "baseplateless_armour_stand");
 
-        ShapedRecipe shapedRecipe = new ShapedRecipe(recipeKey, armourStand);
-        shapedRecipe.shape("SSS", " S ", "S S");
-        shapedRecipe.setIngredient('S', Material.STICK);
+        ShapedRecipe baseplateLessRecipe = new ShapedRecipe(baseplateLessArmourStandRecipeKey, armourStand);
+        baseplateLessRecipe.shape("SSS", " S ", "S S");
+        baseplateLessRecipe.setIngredient('S', Material.STICK);
 
-        Bukkit.addRecipe(shapedRecipe);
-
-        Iterator<Recipe> recipeIterator = getServer().recipeIterator();
-        while (recipeIterator.hasNext()) {
-            Recipe recipe = recipeIterator.next();
-        }
+        Bukkit.addRecipe(baseplateLessRecipe);
     }
 }
---
+++ src/main/java/org/rolypolyvole/armourstandarms/util/RecipeUtil.java
@@ -0,0 +1,21 @@
+package org.rolypolyvole.armourstandarms.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.Recipe;
+
+import java.util.Iterator;
+
+public class RecipeUtil {
+    public static void removeRecipe(Material resultItemType){
+        Iterator<Recipe> recipeIterator = Bukkit.getServer().recipeIterator();
+
+        while(recipeIterator.hasNext()){
+            Recipe recipe = recipeIterator.next();
+
+            if (recipe.getResult().getType().equals(resultItemType)) {
+                recipeIterator.remove();
+            }
+        }
+    }
+}
