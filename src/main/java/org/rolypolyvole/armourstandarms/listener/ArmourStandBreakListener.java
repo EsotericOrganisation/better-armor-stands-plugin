d977c7fb65584c83c80628ffc23b740f1d5be844
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-02-18 19:01:51 +0000

    Drop baseplate when broken

---
+++ src/main/java/org/rolypolyvole/armourstandarms/listener/ArmourStandBreakListener.java
@@ -0,0 +1,21 @@
+package org.rolypolyvole.armourstandarms.listener;
+
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class ArmourStandBreakListener implements Listener {
+
+    @EventHandler
+    public void onArmourStandBreak(@NotNull EntityDeathEvent event) {
+        if (event.getEntity() instanceof ArmorStand stand && stand.hasBasePlate()) {
+            ItemStack smoothStoneSlab = new ItemStack(Material.SMOOTH_STONE_SLAB, 1);
+
+            stand.getWorld().dropItemNaturally(stand.getLocation(), smoothStoneSlab);
+        }
+    }
+}
